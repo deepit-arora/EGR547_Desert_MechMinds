19ff284f084f67481c73be872bdfad0a19090907
Darius <irani@cognitionfactory.com>
2024-04-20 15:45:59 -0700

    Updated forward kinematics, added intertia matrix functions

--- helper_functions/f_kinematics.m
+++ helper_functions/f_kinematics.m
@@ -1,17 +1,23 @@
 %% Generates the forward kinematics T0H cell array with T0H{i} matricies.
-
 function [T0H, T0H_sym] = f_kinematics(thetas, alphas, a, d)
     % This funciton calculates the T0H array (1xn array with n T0H 4x4
     % matricies), from a list of thetas, alphas, a's, and d's. This
     % function returns the calculated T0H and the symbolic T0H, as a
     % tuple.
     
+    % USAGE: 
+    %        input thetas as radians, i.e thetas = [pi/4 0 0]
+    %        input alphas as radians, i.e alphas = [0 pi/2 pi/4]
+    %        input a as numbers, i.e a = [1 0 0]
+    %        input d as numbers, i.e d = [0 1.5 2.5]
+    %     to return the reduced T0H matrix and the symbolic T0H matrix do:
+    %     [T0H, T0H_sym] = f_kinematics(thetas, alphas, a, d)
+
     number_joints = length(thetas);
     if (length(alphas) ~= number_joints) || (length(a) ~= number_joints) || (length(d) ~= number_joints)
         error('The input vectors must have the same length as the number of joints.');
     end
     
-    dh = [thetas' d' a' alphas'];
     
     T = @(theta,alpha,a,d) [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta);
         sin(theta) cos(theta)*cos(alpha) -cos(theta)*sin(alpha) a*sin(theta);
@@ -23,41 +29,52 @@ function [T0H, T0H_sym] = f_kinematics(thetas, alphas, a, d)
     d_sym = cell(1, number_joints);
     a_sym = cell(1, number_joints);
 
+    % getting symbolic joint variables from input list
     for i = 1:number_joints
-        q_list{i} = sym(sprintf('q%d', i));
-        alpha_list{i} = sym(sprintf('alpha%d', i));
-        d_sym{i} = sym(sprintf('d%d', i));
-        a_sym{i} = sym(sprintf('a%d', i));
+        if thetas(i) ~= 0
+            q_list{i} = sym(sprintf('q%d', i));
+        else
+            q_list{i} = 0;
+        end
+        if alphas(i) ~= 0
+            alpha_list{i} = sym(sprintf('alpha%d', i));
+        else
+            alpha_list{i} = 0;
+        end
+        if d(i) ~= 0
+            d_sym{i} = sym(sprintf('d%d', i));
+        else
+            d_sym{i} = 0;
+        end
+        if a(i) ~= 0
+            a_sym{i} = sym(sprintf('a%d', i));
+        else
+            a_sym{i} = 0;
+        end
     end
 
-    % Getting symbolic expression for geometric jacobian
     for i=1:number_joints
-        T0H_sym{i} = eye(4);
+        T0H_sym{i} = zeros(4);
     end
 
-    % Getting T0H array
+    % Creating Symbolic T0H array
     for i=1:number_joints
         if i == 1
             T0H_sym{i} = T(q_list{i},alpha_list{i},a_sym{i}, d_sym{i});
         else
             T0H_sym{i} = T0H_sym{i-1} * T(q_list{i},alpha_list{i},a_sym{i}, d_sym{i});
         end
-    end
-
-    % Simplifying T0H array
-    for i=1:number_joints
         T0H_sym{i} = simplify(T0H_sym{i});
     end
 
-    thetas_sym = arrayfun(@sym, thetas, 'UniformOutput', false);
-    alphas_sym = arrayfun(@sym, alphas, 'UniformOutput', false);
-    a_sym_values = arrayfun(@sym, a, 'UniformOutput', false);
-    d_sym_values = arrayfun(@sym, d, 'UniformOutput', false);
+    % Creating Numerical T0H array
+    for i=1:number_joints
+        T0H{i} = zeros(4);
+    end
 
-    subs_map = [q_list', thetas_sym'; alpha_list', alphas_sym'; a_sym', a_sym_values'; d_sym', d_sym_values'];
-    T0H = cellfun(@(x) subs(x, subs_map(:, 1), subs_map(:, 2)), T0H_sym, 'UniformOutput', false);
-    
-    % Return numeric T0H matrices
-    T0H = cellfun(@double, T0H, 'UniformOutput', false);
+    for i = 1:number_joints
+        T0H{i} = subs(T0H_sym{i}, [q_list, alpha_list, a_sym, d_sym], [thetas, alphas, a, d]);
+        T0H{i} = simplify(T0H{i});
+    end
 
 end
---
+++ helper_functions/inertia_matrix.m
@@ -0,0 +1,137 @@
+%% Derive Intertia Matrix based on DH inputs
+function B_sum = inertia_matrix(thetas, alphas, a, d, jointTypes)
+    % This function calculates the intertia matrix for a given set
+    % of dh parameters, motor masses, and link masses, and returns it
+    % as a 3xn (number of joints) matrix.
+
+    % USAGE:
+    %        input thetas as radians, i.e thetas = [pi/4 0 0]
+    %        input alphas as radians, i.e alphas = [0 pi/2 pi/4]
+    %        input a as numbers, i.e a = [1 0 0]
+    %        input d as numbers, i.e d = [0 1.5 2.5]
+    %        input jointTypes as letters, i.e jointTypes = ['R', 'P', 'P']
+    %    to return intertia matrix B, do:
+    %    B = inertia_matrix(thetas, alphas, a, d, jointTypes)
+    %    B will be the 3xn intertia matrix corresponding to the DH params
+
+
+    % Getting and checking number of joints
+    number_joints = length(thetas);
+    if (length(alphas) ~= number_joints) || (length(a) ~= number_joints) || (length(d) ~= number_joints)
+        error('The input vectors must have the same length as the number of joints.');
+    end
+    
+    % using forward kinematics function to get T0H and T0H syms matricies
+    [T0H, T0H_sym] = f_kinematics(thetas, alphas, a, d);
+
+    % Setting motor and link symbolic masses
+    for i = 1:number_joints
+        ml_list{i} = sym(sprintf('ml_%d', i));
+        mm_list{i} = sym(sprintf('mm_%d', i));
+    end
+    
+    % Setting intertia symbolic variables
+    for i = 1:number_joints
+        Il_list{i} = sym(sprintf('Il_%d', i));
+        Im_list{i} = sym(sprintf('Im_%d', i));
+    end
+
+    % Computation of Kinetic Energy:
+    % Jacobians for Links:
+    syms alpha1 alpha2 d1 d2
+    Jp_l = {};
+    Jo_l = {};
+    for i=1:number_joints
+        Jp_l{i} = sym(zeros(3, number_joints));
+        Jo_l{i} = sym(zeros(3, number_joints));
+
+        % prismatic joints
+        if jointTypes(i) == 'P' || jointTypes(i) == 'p'
+            for j=1:number_joints
+                if j <= i 
+                    Jp_l{i}(:, j) = T0H_sym{j}(1:3, 3);
+                end
+            end
+
+        % revolute joints
+        % origin frame T0H(0) = T0H{1}
+        elseif jointTypes(i) == 'R' || jointTypes(i) == 'r'
+            for j=1:number_joints
+                if j == 1
+                    Jp_l{i}(:, j) = cross(T0H_sym{j}(1:3, 3), T0H_sym{i}(1:3, 4));
+                    Jo_l{i}(:, j) = T0H_sym{j}(1:3, 3);
+                elseif (j ~= 1) && (j <= i)
+                    Jp_l{i}(:, j) = cross(T0H_sym{j}(1:3, 3), T0H_sym{i}(1:3, 4)-T0H_sym{j-1}(1:3, 4));
+                    Jo_l{i}(:, j) = T0H_sym{j}(1:3, 3);
+                end
+            end
+        end
+    end
+    
+    % Jacobians for Motors:
+    Jp_m = {};
+    Jo_m = {};
+ 
+    for i=1:number_joints
+        Jp_m{i} = sym(zeros(3, number_joints));
+        
+        % prismatic joints
+        if jointTypes(i) == 'P' || jointTypes(i) == 'p'
+            for j=1:number_joints
+                if j > i-1
+                    continue
+                else
+                    Jp_m{i}(:, j) = T0H_sym{j}(1:3, 3);
+                end
+            end
+        
+        % revolute joints
+        elseif jointTypes(i) == 'R' || jointTypes(i) == 'r'
+            for j=1:number_joints
+                if j > i-1
+                    continue
+                else
+                    if j == 1
+                        Jp_m{i}(:, j) = cross(T0H_sym{j}(1:3, 3), T0H_sym{i}(1:3, 3));
+                    else
+                        Jp_m{i}(:, j) = cross(T0H_sym{j-1}(1:3, 3), T0H_sym{i}(1:3, 4) - T0H_sym{j-1}(1:3, 4));
+                
+                    end
+                end
+            end
+        end
+    end
+
+    for i = 1:number_joints
+        Jo_m{i} = sym(zeros(3, number_joints));
+        krm_i = sym(sprintf('krm%d', i));
+        for j=1:number_joints
+            if j < i
+                Jo_m{i}(:, j) = Jo_l{i}(:, j);
+            elseif j==i
+                Jo_m{i}(:, j) = krm_i*T0H{i}(1:3, 3);
+            end
+        end
+    end
+
+    % Finding the B(q) matrix from the sum equation:
+    for i=1:number_joints
+        R_i_l = T0H_sym{i}(1:3, 1:3);
+        if i==1
+            R_i_m = [[1 0 0]' [0 1 0]' [0 0 1]'];
+        else
+            R_i_m = T0H_sym{i-1}(1:3, 1:3);
+        end
+        B{i} = ml_list{i}*transpose(Jp_l{i})*Jp_l{i} + ...
+                 transpose(Jo_l{i})*R_i_l*Il_list{i}*transpose(R_i_l)*Jo_l{i} + ...
+                 mm_list{i}*transpose(Jp_m{i})*Jp_m{i} + ...
+                 transpose(Jo_m{i})*R_i_m*Im_list{i}*transpose(R_i_m)*Jo_m{i};
+    end
+
+    B_sum = zeros(size(B{1}, 1), size(B{1}, 2));
+    for i=1:3
+        B_sum = simplify(B_sum + B{i});
+    end
+    
+end
+
