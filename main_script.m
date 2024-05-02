a1f209859874dab5ab765af496a7ec6466c105db
deepit <deepitarora2002@gmail.com>
2024-05-01 19:41:02 -0700

    update with gui and added coorke toolbox

--- main_script.m
+++ main_script.m
@@ -1,5 +1,11 @@
-% Main script
+% MAE 547 Final Project
+% run this script to access the GUI
+clear all
+disp("Welcome to MAE547 Final Project. Refer to the GUI screen for next steps")
+warning('off')
 addpath(".\helper_functions");
+addpath(".\rvctools");
+run("startup_rvc.m")
 hGui = gui_guide;
 handles = guidata(hGui);
 guidata(hGui, handles);
